9ca857ca8c238b0f67c3983f989ac29d6f99049a
tnarik <tnarik@gmail.com>
2014-05-03 17:01:09 +0100

    Builder names updated to standards. Variables used for ISOs. Added output base directory for result machines. Headless mode enabled. Scripts modified to simplify them an rely instead on the packer system to install guest additions and such.

--- scripts/base.sh
+++ scripts/base.sh
@@ -10,7 +10,5 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 #gpgcheck=0
 #EOM
 
-#yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
-
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,11 +1,12 @@
 #yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
-yum -y install gcc make kernel-devel-`uname -r` perl dkms
+
+rpm -Uhv http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
+yum -y install gcc make kernel-devel perl dkms
 
 # Installing the virtualbox guest additions
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
-
+#rm -rf /home/vagrant/VBoxGuestAdditions.iso
